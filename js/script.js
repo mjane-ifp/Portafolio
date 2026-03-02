8c5d523cebb947f6111b77b00f0041635c08da51
Miki <mjane@ifp-alguero.org>
2026-03-03 00:26:21 +0100

    canvis finals

--- js/script.js
+++ js/script.js
@@ -127,34 +127,3 @@ scrollBtn.addEventListener("click", () => {
     });
 });
 
-
-// =============================
-// HOVER 3D EFFECT A LES CARDS
-// =============================
-const cards = document.querySelectorAll(".project-card");
-
-cards.forEach(card => {
-    card.addEventListener("mousemove", e => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-
-        const rotateX = (y - centerY) / 15;
-        const rotateY = (centerX - x) / 15;
-
-        card.style.transform = `
-            rotateX(${rotateX}deg)
-            rotateY(${rotateY}deg)
-            scale(1.03)
-        `;
-        card.style.transition = "none";
-    });
-
-    card.addEventListener("mouseleave", () => {
-        card.style.transform = "rotateX(0) rotateY(0) scale(1)";
-        card.style.transition = "all 0.4s ease";
-    });
-});
